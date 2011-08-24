dd780976617910808aca901c68928abe37d8cc23
Vanya <vanya.iliyn@yahoo.com>
2011-08-25 01:17:48 +0400

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
 
-# Return a title on a per-page basis.               # Documentation comment
+
+
+  def logo
+  logo = image_tag("logo.png")
+
+  end
+  # Return a title on a per-page basis.               # Documentation comment
   def title                                           # Method definition
     base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
     if @title.nil?                                    # Boolean test for nil
